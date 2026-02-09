5d48d72cee6b129b9b4509eec85a4d1d910c275b
Claire Zhang <clairez2015@gmail.com>
2026-02-09 14:21:34 +1100

    attempt to add hamburger to navbar mobile, not complete

---
+++ scripts/navbar.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+})
+
+document.querySelector('.nav-link').addEventListener('click', () => {
+  addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navLinks.classList.remove('active');
+  })
+})
